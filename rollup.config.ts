f6ef29e3c89e54a9ccb1115c3344753375cd44a6
jumodada <864353360@qq.com>
2020-03-12 21:38:12 +0800

    add typedoc

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from 'rollup-plugin-json'
 
 const pkg = require('./package.json')
 
-const libraryName = 'date-picker'
+const libraryName = 'axios'
 
 export default {
   input: `src/index.ts`,
