0f0f7ae037fddeec4ea06363e1ca3e72707188e2
jumodada <864353360@qq.com>
2020-03-21 12:36:36 +0800

    从vue上抄了nexttick

--- src/popover/index.ts
+++ src/popover/index.ts
@@ -2,51 +2,53 @@ import Store from "../store"
 import flexOptions from "../types/options"
 import {positionAttr, _spm} from "../types/popover"
 import nextTick from '../utils/nexttick'
+const  transform = {
+    top: `translate(0,-100%)`,
+    left: `translate(-100%,0)`,
+    bottom: `translate(0,0)`,
+    right: `translate(0,0)`,
+}
+
 export function createPopover() {
     const _pop = document.createElement('div')
     Store._updatePop(_pop)
 }
-
-
 export function updatePopover(el:HTMLElement,value:boolean):void {
     if(value){
-        setPosition(el)
+        setPopoverLocation(el)
         el.style.display = 'block'
     }else{
         el.style.display = 'none'
     }
 }
 
-export function setPosition(el:HTMLElement) {
+export function setPopoverLocation(el:HTMLElement) {
+    const placement = (Store._getOptions() as flexOptions).placement.split('-')[0] as _spm
+    setPosition(el,placement)
+    setTransform(el,placement)
+}
+export function setTransform(el:HTMLElement,placement:any){
+    nextTick(() => {
+        el.style.transform = transform[placement as _spm]
+    })
+}
+
+export function setPosition(el:HTMLElement,placement:_spm) {
     const reference = Store._getReference()
     let {top, left, height, width} = reference.getBoundingClientRect()
-    let _tTop, _rTop, _tLeft, _rLeft
+    let _tTop, _bTop, _tLeft, _rLeft
     _tTop = top + window.scrollY
-    _rTop = top + height + window.scrollY+3
+    _bTop = top + height + window.scrollY+3
     _tLeft = left + window.scrollX
     _rLeft = left + width + window.scrollX
-    const placement = (Store._getOptions() as flexOptions).placement.split('-')[0]
     const position = {
         top: {top: _tTop, left: _tLeft},
         left: {top: _tTop, left: _tLeft},
-        bottom: {top: _rTop, left: _rLeft},
+        bottom: {top: _bTop, left: _rLeft-width},
         right: {top: _tTop, left: _rLeft}
     }
     Array.from(['left', 'top'])
         .forEach(attr => el.style[attr as any] =
             position[placement as _spm]
                 [attr as positionAttr] + 'px')
-    nextTick(()=>{
-        let contentHeight = el.clientHeight
-        let transform = {
-            top: `translate(0,-100%)`,
-            left: `translate(-100%,0)`,
-            bottom: `translate(-${width}px,0)`,
-            right: `translate(0,0)`,
-        }
-        el.style.transform = transform[placement as _spm]
-    })
-    nextTick(()=>{
-        console.log('第二个')
-    })
 }
--- src/utils/nexttick.ts
+++ src/utils/nexttick.ts
@@ -1,9 +1,11 @@
-
-import {isIE} from "./env"
-
-const callbacks: any[] = []
+// https://github.com/vuejs/vue/blob/dev/src/core/util/next-tick.js
+import { isIE, isNative } from './env'
+/*
+**在原有的vue-nextTick上面移除了对ios的兼容。
+*/
+const callbacks:any[] = []
 let pending = false
-let timerFunc:any
+
 function flushCallbacks () {
     pending = false
     const copies = callbacks.slice(0)
@@ -13,14 +15,61 @@ function flushCallbacks () {
     }
 }
 
-timerFunc = ()=>{
-    console.log('nn')
-    setTimeout(flushCallbacks, 0)
+let timerFunc:()=>void
+
+if (typeof Promise !== 'undefined' && isNative(Promise)) {
+    const p = Promise.resolve()
+    timerFunc = () => {
+        p.then(flushCallbacks)
+    }
+} else if (!isIE && typeof MutationObserver !== 'undefined' && (
+    isNative(MutationObserver) ||
+    MutationObserver.toString() === '[object MutationObserverConstructor]'
+)) {
+    let counter = 1
+    const observer = new MutationObserver(flushCallbacks)
+    const textNode = document.createTextNode(String(counter))
+    observer.observe(textNode, {
+        characterData: true
+    })
+    timerFunc = () => {
+        counter = (counter + 1) % 2
+        textNode.data = String(counter)
+    }
+} else if (typeof setImmediate !== 'undefined' && isNative(setImmediate)) {
+    timerFunc = () => {
+        setImmediate(flushCallbacks)
+    }
+} else {
+    timerFunc = () => {
+        setTimeout(flushCallbacks, 0)
+    }
 }
 
-export default function nextTick(cb:any) {
-    console.log(isIE)
-    setTimeout(()=>{
-        cb()
-    }, 0)
+export default function nextTick (cb?: Function, ctx?: Object) {
+    let _resolve:(ctx:any)=>void
+    callbacks.push(() => {
+        if (cb) {
+            try {
+                cb.call(ctx)
+            } catch (e) {
+                console.error('nexttick callback error')
+            }
+        } else if (_resolve) {
+            _resolve(ctx)
+        }
+    })
+    if (!pending) {
+        pending = true
+        timerFunc()
+    }
+    if (!cb && typeof Promise !== 'undefined') {
+        return new Promise(resolve => {
+            _resolve = resolve
+        })
+    }
+}
+
+export {
+    nextTick
 }
