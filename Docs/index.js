4e96767b62b6064be94683e288ccbe2c479eb08b
jumodada <864353360@qq.com>
2020-03-31 18:08:56 +0800

    add gulp

--- Docs/index.js
+++ Docs/index.js
@@ -6,7 +6,7 @@ import highlightJs from 'highlight.js'
 import './assets/styles/index.scss'
 import Icon from './components/icon'
 import Input from './components/Input'
-import '../src/assets/date-time-picker.scss'
+import '../src/assets/src/date-time-picker.scss'
 Vue.component(demoCard.name, demoCard)
 Vue.component(Input.name, Input)
 Vue.component(Icon.name,Icon)
--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,41 +1,34 @@
 const path = require('path')
-const ProgressBarPlugin = require('progress-bar-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const isProd = process.env.NODE_ENV === 'production'
-const webpackConfig = {
-    mode: process.env.NODE_ENV,
-    entry:'./src/assets/date-time-picker.scss',
-    output:{
-        filename:'style.css',
-        path:path.join(__dirname,'./css-dist')
-    },
-    resolve: {
-        modules: ['node_modules'],
-    },
-    performance: {
-        hints: false
-    },
-    stats: {
-        children: false
-    },
 
+module.exports = {
+    entry: './src/assets/date-time-picker.scss',
+    output: {
+        filename: 'style.css',
+        path: path.resolve(__dirname, './dist'),
+    },
     module: {
         rules: [
             {
-                test: /\.(scss|css)$/,
+                test: /\.scss$/,
                 use: [
-                    isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-                    'css-loader',
-                    'sass-loader'
+                    {
+                        loader: "style-loader" // 将 JS 字符串生成为 style 节点
+                    },
+                    {
+                        loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
+                    },
+                    {
+                        loader: "sass-loader" // 将 Sass 编译成 CSS
+                    }
                 ]
-            },
+            }
         ]
     },
     plugins: [
-        new ProgressBarPlugin(),
-        new MiniCssExtractPlugin("styles.css")
+        new MiniCssExtractPlugin({
+            filename: '../dist/css/style.css',
+        }),
     ],
-}
-
-
-module.exports = webpackConfig
+    mode: 'development' // 设置mode
+};
