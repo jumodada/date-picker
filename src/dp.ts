045ec7b6a7e8365cc75b05bb7d02b23923eb579b
jumodada <864353360@qq.com>
2020-07-09 22:20:34 +0800

    fixed:Arrow function has no arguments

--- src/dp.ts
+++ src/dp.ts
@@ -5,10 +5,9 @@ import InitOptions from "./initial-options"
 import './svg'
 
 function createInstance(config: FlexOptions):any {
-    return (el:HTMLElement,options:FlexOptions)=>{
+    return function(el:HTMLElement,options:FlexOptions){
         const context = new Flex(config)
-        // @ts-ignore
-        const instance = Flex.create.apply(context,arguments)
+        const instance = Flex.create.apply(context,arguments as any)
         extend(context,instance)
         return instance
     }
